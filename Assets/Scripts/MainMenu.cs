3c42c200e5f0276ef092adbb264be94e4fcc1b17
Pewners <jfadikpe2021@elam.co.uk>
2022-12-25 16:12:40 +0000

    programmed main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void Easy()
+	{
+		SceneManager.LoadScene(1);
+	}
+	
+	public void Medium()
+	{
+		//SceneManager.LoadScene(Medium);
+		Debug.Log("Medium");
+	}
+	
+	public void Hard()
+	{
+		//SceneManager.LoadScene(Hard);
+		Debug.Log("Hard");
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("Quit");
+		Application.Quit();
+	}
+}
