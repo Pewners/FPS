314343e38098a1daafeb442d84b378ef22028d25
Pewners <jfadikpe2021@elam.co.uk>
2022-12-25 16:59:37 +0000

    target

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    float rotation = 0.0f;
+    float camRotation = 0.0f;
+    float rotationSpeed = 2.0f;
+    float camRotationSpeed = 1.5f;
+    GameObject cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+
+        cam = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+
+        camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+        cam.transform.localRotation = Quaternion.Euler(new Vector3(Mathf.Clamp(-camRotation, -45f, 30f), -90f, 0.0f));
     }
 }
